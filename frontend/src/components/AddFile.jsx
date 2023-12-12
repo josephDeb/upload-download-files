5d8a4000f5c6b4cb687a89817f852b28149c44d5
Joseph dat Dev <139043824+josephDeb@users.noreply.github.com>
2023-12-12 09:23:45 -0300

    handleDelete

--- frontend/src/components/AddFile.jsx
+++ frontend/src/components/AddFile.jsx
@@ -2,34 +2,37 @@ import React, { useState } from 'react'
 import axios from 'axios'
 const AddFile = ({ons, setOn}) => {
 
-    const [values, setValues] = useState({
-        name: "",
-        file: ""
-    })
 
+    const [file, setFile] = useState("")
+    const [name, setName] = useState("")
     const handleAdd = async () => {
-        await axios.post("http://localhost:8000/api/items", values)
+        const formData = new FormData()
+        formData.append("name", name)
+        formData.append("file", file)
+        await axios.post("http://localhost:8000/api/items", formData)
         .then(res => {
             console.log(res)
             alert("Create")
         })
     }
 
+
+
   return (
     <div  className='max-w-screen-2xl mx-auto flex justify-center items-center'>
         <div className={` ${ons ? "fixed z-[62]" : "hidden"} w-[71%] xl:w-[44%] mx-auto h-[53%] bg-black/80 top-[28%] flex flex-col justify-center items-center container`}>
             <h1 className='text-white text-4xl mb-5'>Add new files</h1>
-            <form className='w-[80%] h-[62%] text-white flex justify-between flex-col '>
+            <form onSubmit={handleAdd} className='w-[80%] h-[62%] text-white flex justify-between flex-col '>
                 <div className='flex flex-col'>
                   <label>Name of file</label>
-                  <input onChange={(e) => setValues({...values, name: e.target.value})} type='text'  className='h-[35px] px-3 text-black'></input>
+                  <input id='name' name='name' onChange={(e) => setName(e.target.value)} type='text'  className='h-[35px] px-3 text-black'></input>
                 </div>
 
                 <div className='flex flex-col'>
-                  <input onChange={(e) => setValues({...values, file: e.target.files[0]})} type='file' className='h-[35px]'></input>
+                  <input id='file' name='file' onChange={(e) => setFile(e.target.files[0])} type='file' className='h-[35px]'></input>
                 </div>
 
-                <button className='w-full bg-blue-500 py-3'>Submit</button>
+                <button className='w-full bg-blue-500 py-3 cursor-pointer'>Submit</button>
             </form>
         </div>
     </div>
--- frontend/src/components/Section.jsx
+++ frontend/src/components/Section.jsx
@@ -1,6 +1,9 @@
 import axios from 'axios'
 import { useEffect, useState } from 'react'
 
+import trash from '../../public/trash.png';
+import download from '../../public/download.png';
+import file from '../../public/google-docs.png';
 // Import Swiper styles
 import 'swiper/css';
 import 'swiper/css/scrollbar';
@@ -19,15 +22,30 @@ const Section = () => {
             console.log(data)
         })
     }, [data.length])
+
+    const handleDelete = async (id) => {
+        axios.delete("http://localhost:8000/api/items/"+id)
+        .then(res => {
+          console.log(res.data)
+          window.location.reload()
+        })
+    }
+    const handleDownload = async (id) => {
+      axios.get("http://localhost:8000/api/items/"+id)
+      .then(res => {
+        console.log(res.data)
+        window.location.reload()
+      })
+  }
   return (
-    <div className='max-w-screen-2xl mx-auto flex justify-center items-center border-2 h-[530px] w-full'>
+    <div className='max-w-screen-2xl mx-auto flex justify-center items-center h-[530px] w-full'>
 
         
         <Swiper
          breakpoints={{
-            320: {slidesPerView: 1, spaceBetween: 12},
+            320: {slidesPerView: 2, spaceBetween: 12},
             640:{slidesPerView: 4, spaceBetween: 32},
-            1260:{slidesPerView: 4, spaceBetween: 17},
+            1260:{slidesPerView: 5, spaceBetween: 17},
           }}
           className='w-full mySwiper h-[62%]'
           scrollbar={{
@@ -36,8 +54,13 @@ const Section = () => {
           modules={[Scrollbar]}
         >
             {data.map((dt, i) => {
-            return <SwiperSlide key={i} className='h-full w-full flex justify-center items-center'>
-           <img src={'http://localhost:8000/Images/'+dt.file} className='object-fill h-full w-full rounded-full hover:rounded-none'/>
+            return <SwiperSlide key={i} className='h-full w-full flex justify-center items-center relative'>
+              <img src={file} className='object-cover'/>
+
+              <div className='absolute h-full w-full flex justify-center items-center gap-5 opacity-0 hover:opacity-100 transition-all duration-500'>
+                 <img src={download} className='w-12'/>
+                 <img onClick={() => handleDelete(dt._id)} src={trash} className='w-12'/>
+              </div>
           </SwiperSlide>
            })}
         </Swiper>
