5170450a1c7b1c01048302daaad969932862bc73
Joseph dat Dev <139043824+josephDeb@users.noreply.github.com>
2023-12-12 17:53:51 -0300

    g

--- frontend/src/components/Section.jsx
+++ frontend/src/components/Section.jsx
@@ -67,7 +67,7 @@ const Section = () => {
               <img src={file} className='object-cover'/>
 
               <div className='absolute h-full w-full flex justify-center items-center gap-5 opacity-0 hover:opacity-100 transition-all duration-500'>
-                <img onClick={() => handleDownload("http://localhost:8000/Images/"+dt.file)}  src={download} className='xl:w-12 cursor-pointer w-8'/>
+                <img onClick={() => handleDownload("https://upload-download-files-g8oe.vercel.app/"+dt.file)}  src={download} className='xl:w-12 cursor-pointer w-8'/>
                  <img onClick={() => handleDelete(dt._id)} src={trash} className='xl:w-12 cursor-pointer w-8'/>
               </div>
           </SwiperSlide>
